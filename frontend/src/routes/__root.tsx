bb6a6896347cce18180b98d28e15643756fcef54
kyooosukedn <danielpurnomo50@gmail.com>
2025-03-21 07:11:02 +0100

    distric prof min

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -19,13 +19,6 @@ const links = [
     ),
   },
   {
-    label: 'District Profile',
-    href: '/district-profile',
-    icon: (
-      <LandPlot className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
-  },
-  {
     label: 'Reports',
     href: '/comparison',
     icon: (
